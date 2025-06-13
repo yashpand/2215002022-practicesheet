68116f16428cc17ea7336ba22de0942d55c24f51
my <yash.sharma83076@gmail.com>
2025-06-13 11:17:28 +0530

    oops public vs static

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 public class Main {
-  int x = 5;
-
+   void myMethod() {
+    System.out.println("Hello World!");
+    my();
+  }
+  static void my(){
+    System.out.println("Hello from my method!");
+    }
   public static void main(String[] args) {
-    Main myObj1 = new Main();  // Object 1
-    Main myObj2 = new Main();  // Object 2
-    myObj2.x = 25;
-    System.out.println(myObj1.x);  // Outputs 5
-    System.out.println(myObj2.x);  // Outputs 25
+    Main main = new Main();
+    main.myMethod();
+    my();
   }
 }
+
+// Outputs "Hello World!"
