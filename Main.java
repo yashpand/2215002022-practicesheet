5c8e5f7f8e5145f9b127771a96c3a8c2805e4a0b
my <yash.sharma83076@gmail.com>
2025-06-13 10:54:38 +0530

    hii

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    myObj2.x = 25;
+    System.out.println(myObj1.x);  // Outputs 5
+    System.out.println(myObj2.x);  // Outputs 25
+  }
+}
