d98ac4b9e697628a0b87091274698e08276585e7
my <yash.sharma83076@gmail.com>
2025-06-18 13:15:59 +0530

    arrays

--- arrays/level2/code4.java
+++ arrays/level2/code4.java
@@ -6,33 +6,33 @@ public class code4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read the number from user
+        
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
-        int maxDigit = 10; // initial size
+        int maxDigit = 10; 
         int[] digits = new int[maxDigit];
         int index = 0;
 
-        // Extract digits and store in array
+     
         while (num > 0) {
             if (index == maxDigit) {
-                // Increase maxDigit by 10
+               
                 maxDigit += 10;
-                // Create new temp array
+               
                 int[] temp = new int[maxDigit];
-                // Copy old digits to temp
+                
                 for (int i = 0; i < digits.length; i++) {
                     temp[i] = digits[i];
                 }
-                // Assign temp to digits
+                
                 digits = temp;
             }
             digits[index++] = num % 10;
             num /= 10;
         }
 
-        // Find largest and second largest
+       
         int largest = -1, secondLargest = -1;
         for (int i = 0; i < index; i++) {
             if (digits[i] > largest) {
